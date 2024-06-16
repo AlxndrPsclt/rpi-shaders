6db35ee3002193f25bd258d1e424474081ad69d5
alex <alex@simulated-generation-005.my.domain>
2024-06-16 22:07:10 +0200

    Cyberpunk version

--- resources/shaders/glsl100/myshader.glsl
+++ resources/shaders/glsl100/myshader.glsl
@@ -119,13 +119,9 @@ void main()
     //float grid = drawGrid(10.0*(uv+noise(fract(uv.x*uv.x))*noise(noise(fract(uv.x)*20.0)*6.0*uv.y)), vec2(0.0,time));
     //float grid = drawGrid(uv+vec2(0.0, noise(sin(time/5.0))*10.0*noise(time/17.77)*noise(noise(time/6.0)*uv.x*noise(uv.x*uv.y)*0.5*floor(uv.y)*noise(50.0*uv.y))), vec2(0.0,noise(5.0*uv.y)*noise(3.0*uv.x)*cos(5.0*uv.x*noise(time))));
 
-    float scanLineSDF = sdSegment( uv/5.0, vec2(sin(0.08*PI*time/10.0)/2.0,-1.0), vec2(sin(0.06*PI*time/10.1)/2.0, 1.0));
-    float scanline = 1.0-smoothstep(0.001,0.0011,abs(scanLineSDF));
 
     float smoothLowfreqs = smoothstep(0.1,2.5,lowfreqs);
     //float smoothLowfreqs2 = smoothstep(1.0,5.0,lowfreqs*(cos(time)+1.0)/2.0);
-    float possibleToShowWave = (smoothstep(0.2,0.4,noise(time))+0.05);
-    float smoothLowfreqs2 = smoothstep(1.0,5.0,lowfreqs*possibleToShowWave);
 
 
 
@@ -136,7 +132,6 @@ void main()
     //float lineSDF5 = sdSegment( uv-vec2(0.0,0.3), vec2(-1.0,sin((0.09+noise(5.0 * bassMagnitude)/1000.0)*PI*time)/2.0), vec2(1.0,sin((0.05+noise(2.0*bassMagnitude)/1000.0)*PI*time)/2.0));
     //float lineSDF6 = sdSegment( uv-vec2(0.0,0.45), vec2(-1.0,sin((0.06+noise(6.0*bassMagnitude)/1000.0)*PI*time)/2.0), vec2(1.0,sin((0.05+noise(bassMagnitude)/1000.0)*PI*time)/2.0));
     //
-    float ctime = time*0.1+smoothLowfreqs2;
     float fastime = time*15.0;
 
     float lineSDF;
@@ -145,10 +140,17 @@ void main()
     //vec2 onde = vec2(0.0, sin(uv.x*10.0*PI-fastime)/12.5)*smoothLowfreqs2;
     //vec2 onde = vec2(0.0, (2.0*noise(3.0*uv.x+fastime)+3.0*sin(uv.x)/uv.x)/12.5)*smoothLowfreqs2;
 
+    float possibleToShowWave;
+    float smoothLowfreqs2;
+    float ctime;
 
-    for(float i = 0.0; i < 10.0; ++i) {
-      onde = vec2(0.0, cos(uv.x*3.33*PI-fract(i*fastime))*sin(uv.x*10.0*i*PI-(i*fastime)/12.5)*smoothLowfreqs2;
-      lineSDF = sdSegment(uv + onde, vec2(0.0,abs(sin((i+ctime)/6.0))), vec2(1.0,abs(sin((i+ctime)/6.0))));
+    for(float i = 0.0; i < 7.0; ++i) {
+      possibleToShowWave = (smoothstep(0.8,0.9,noise(7.333*(i+1.0)*time))+0.05);
+      smoothLowfreqs2 = smoothstep(1.0,5.0,lowfreqs*possibleToShowWave);
+
+      onde = vec2(0.0, cos(noise(i*time)*uv.x*3.33*PI-fract(fastime))*sin(uv.x*10.0*i*PI-fastime)/12.5)*smoothLowfreqs2;
+      ctime = time*0.1+smoothLowfreqs2;
+      lineSDF = sdSegment(uv + onde, vec2(0.0,fract(time/10.0+abs(sin((i+ctime)/6.0)))), vec2(1.0,abs(sin((i+ctime)/6.0))));
       //lineSDF = sdSegment( uv, vec2(-1.0,abs(cos(lowfreqs)+sin((i+ctime)/6.0))), vec2(1.0,noise(uv.y)+abs(sin(lowfreqs)+sin((i+ctime)/6.0))));
       nline += 1.0-smoothstep(0.002,0.0021,abs(lineSDF));
     }
@@ -165,6 +167,9 @@ void main()
 //    nline += 1.0-smoothstep(0.002,0.0021,abs(lineSDF5));
 //    nline += 1.0-smoothstep(0.002,0.0021,abs(lineSDF6));
 
+    float scanLineSDF = sdSegment( uv/5.0, vec2(sin(0.08*PI*time/10.0)/2.0,-1.0), vec2(sin(0.06*PI*time/10.1)/2.0, 1.0));
+    float scanline = 1.0-smoothstep(0.0001,0.00011*(1.0+smoothLowfreqs2),abs(scanLineSDF));
+
 
 
     //vec3 color = 3.0*grid*vec3(3.0*noise(uv.y)*noise(uv.x)*noise(time*sin(time)), 0.01, noise(2.0*uv.y));
@@ -173,15 +178,22 @@ void main()
     vec3 scanLineColored=scanline*vec3(0.2,0.7,3.0);
 
     color+=scanLineColored;
-    color+=nline*vec3(0.0,0.0,5.0);
+    color+=nline*vec3(2.0*smoothLowfreqs2,0.1,0.1)*1.5;
     //color+=nline*vec3(2.6,0.4,0.0); // GREAT yellow cyberpunk color;  2.0*color in gl_FragColor
     //color+=nline*vec3(1.6,0.1,2.0)*vec3(noise(noise(uv.x)),noise(noise(uv.y)),noise(time));
     //color+=soundwaveValue;
     //color+=grid;
 
-    color*=lowfreqs;
+    //color*=lowfreqs;
 
     //vec3 sound = vec3(sampleValue);
+    //float wanderingLineSDF = sdSegment(uv, vec2(0.0,abs(sin(ctime/2.0))), vec2(1.0,abs(sin(ctime/2.0))));
+    float wanderingLine=0.0;
+    float wanderingLineSDF = sdSegment(uv, vec2(0.0,sin(time*0.1)), vec2(1.0,cos(time*0.1)));
+    wanderingLine += 1.0-smoothstep(0.004,0.0045,abs(wanderingLineSDF));
+    vec3 wanderingLineColored = wanderingLine * vec3(0.0,0.5,0.1)*(1.0-smoothLowfreqs2);
+
+    color+=wanderingLineColored;
 
     //gl_FragColor = vec4(4.0*color*BEAT4, 1.0);
     gl_FragColor = vec4(3.0*color, 1.0);
