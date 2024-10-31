3afeb3ccaa8fd59699f60f8b6a656f6ad4598d6d
alxndrpsclt <alxndr.psclt@gmail.com>
2024-10-31 02:25:14 +0100

    Ports reactive cyberpunk shader to multi screen

--- resources/shaders/glsl100/cyberpunk2024-002.glsl
+++ resources/shaders/glsl100/cyberpunk2024-002.glsl
@@ -67,32 +67,24 @@ void main() {
     float F21=zeroctl_F21;
 
     vec2 uv = (gl_FragCoord.xy / resolution.xy);  // Normalize the screen coordinates
-    vec2 uvScaled =(uv-vec2(0.5, 0.5));
-                                                 
     //vec2 uvtex = (gl_FragCoord.xy - vec2(F14,F15) / resolution.xy);  // Normalize the screen coordinates
-    vec2 cell= floor(NB_CELLULES*uvScaled);
-    vec2 uvScaledCell =(uv-(cell+0.5))*F17*1.0;
-    vec2 cellCentered= floor(NB_CELLULES*uvScaledCell);
     
     float dispX = F15*F15*F15*F15;
     float dispY = F16*F16*F16*F16;
-    vec4 prevColor = texture2D(prevFrame, uvScaled-vec2(dispX,dispY));
+    vec4 prevColor = texture2D(prevFrame, uv-vec2(dispX,dispY));
     // Use oscFloat to adjust the color based on time
     //vec3 color = vec3(F11 * uv.x, F12 * uv.y, abs(sin(time * F13)));
 
-
-    vec2 incellCoord= fract(15.0*uv);
-    ///vec2 displayCell=smoothstep(0.6,0.7,sin(incellCoord));
-
+    vec2 cell= floor(NB_CELLULES*uv);
+    //vec2 incellCoord= fract(15.0*uv);
+    //vec2 displayCell=smoothstep(0.6,0.7,sin(incellCoord));
     float saturation = 0.0;
-    vec3 voisins = vec3(0.0);
     for (float i = 0.0; i < 4.0; i += 1.0) {
       for (float j = 0.0; j < 4.0; j += 1.0) {
-        voisins += texture2D(prevFrame, vec2(0.125+0.25*i,0.125+0.25*j)).rgb;
+        saturation += length(texture2D(prevFrame, vec2(0.125+0.25*i,0.125+0.25*j)).rgb);
       }
     }
-    voisins /= 16.0;
-    saturation = length(voisins);
+    saturation /= 16.0;
 
     float cs=1.0/NB_CELLULES;
     vec4 prevColorN = texture2D(prevFrame, uv+vec2(0.0,cs));
@@ -116,42 +108,15 @@ void main() {
     float composanteG = (prevColorN.g + prevColorNE.g + prevColorE.g + prevColorSW.g)/4.0;
     float composanteB = (prevColorNW.b + prevColorNE.b + prevColorE.b + prevColorS.b)/4.0;
 
-    vec4 pointBinaire = vec4(composanteR, composanteG, composanteB, 1.0);
+    vec4 pointBinaire = vec4(randomFF(composanteR), randomFF(composanteG), randomFF(composanteB), 1.0);
     
     // Use oscInt to influence brightness (scaling factor)
     vec3 colorGrid = vec3(uv.x, uv.y, 0.0);
     vec3 color = vec3(F11, F12, F13);
     
-    vec4 finalColor = vec4((1.0+F14/10.0)*prevColor.xyz +color*point, 1.0)+0.06*noise(time)*(pointVoisinEN+pointVoisinWS+pointVoisinWN+pointVoisinES);
+    vec4 finalColor = vec4((1.0+F14/10.0)*prevColor.xyz +color*point, 1.0)+0.06*sin(time)*(pointVoisinEN+pointVoisinWS+pointVoisinWN+pointVoisinES);
     finalColor= vec4(step(0.1,length(finalColor.rgb))*finalColor.rgb, 1.0);
     float sstepSaturation =smoothstep(1.5,1.74,saturation);
     float stepSaturation =step(1.3,saturation);
-
-
-//G vR ->R
-//B vG ->G
-//R vB ->B
-//
-//G vG ->B
-//B vB ->R
-//R vR ->G
-
-    vec3 normalizedV = normalize(finalColor.rgb);
-    // Diagonal direction of the cube
-    vec3 diagonal = normalize(vec3(1.0, 1.0, 1.0));
-
-    // Calculate the dot product between normalizedV and diagonal
-    float alignment = dot(normalizedV, diagonal);
-
-    //
-
-    vec3 finalColor3 =(1.0-sstepSaturation)*finalColor.rgb- 2.0*(courbeExp(alignment))*pointBinaire.rgb;
-
-    normalizedV = normalize(finalColor3);
-    alignment = dot(normalizedV, diagonal);
-    float point2 = step(courbeExp(F19),randomFF(randomFF(3.0*cellCentered.x*fract(uv.x))+randomFF(3.0*cellCentered.y*fract(uv.y))*floor(time)));
-
-    finalColor3 -= (courbeExp(alignment))*point2*vec3(0.3*randomFF(uv.x*sin(time)), 0.1*randomFF(uv.y*cos(time)),0.7*randomFF(uv.y*tan(time)));
-
-    gl_FragColor = vec4(finalColor3, 1.0);
+    gl_FragColor = vec4((1.0-sstepSaturation)*finalColor.rgb- stepSaturation*pointBinaire.rgb, 1.0);
 }
